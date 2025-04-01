bbe150c9102c3e214f9af58d51e19efea96584bd
Mateus Sombrio <mateusombrio@gmail.com>
2025-03-31 22:51:23 -0300

    Trying to finish

--- script.js
+++ script.js
@@ -2,51 +2,74 @@ const inputText = document.querySelector(".input-text");
 const divTask = document.querySelector(".div_tasks");
 const divTaskList = document.querySelector(".div_task_list");
 const arrow = document.querySelector(".arrows");
+const divCheckbox = document.querySelector(".div_checkbox");
+const divValue = document.querySelector(".div_value");
+const urlCheckboxDone = "assets/done.png";
+const urlCheckboxNotDone = "assets/not-done.png";
+const clear = document.querySelector(".clear");
 
-/* Showing and hidding the Task Div */
-divTaskList.addEventListener("click", function () {
-  if (divTask.style.display == "none") {
-    divTask.style.display = "block";
+function toggleTaskDiv() {
+  if (divTask.style.display === "none" || divTask.style.display === "") {
+    divTask.style.display = "flex";
     arrow.src = "assets/arrow-up.png";
   } else {
     divTask.style.display = "none";
     arrow.src = "assets/arrow-down.png";
   }
-});
+}
 
-/* Get the input value */
-const divCheckbox = document.querySelector(".div_checkbox");
-const divValue = document.querySelector(".div_value");
+function createTask(taskText) {
+  const taskIndex = localStorage.length;
+  localStorage.setItem(`task${taskIndex}`, taskText);
 
-const getInputValue = inputText.addEventListener("keypress", function () {
-  let count = 0;
-  if (event.key == "Enter") {
-    const inputValue = inputText.value;
-    /* LocalStorage */
-    const setTask = localStorage.setItem("task" + count, inputValue);
-    const getTask = localStorage.getItem("task" + count);
-    /* Create Div Checkbox and Value */
-    const firstDiv = document.createElement("div");
-    firstDiv.className = "div_checkbox";
-    const secondDiv = document.createElement("div");
-    secondDiv.className = "div_value";
+  const taskDiv = document.createElement("div");
+  taskDiv.className = "task-item";
 
-    /* Add Div inside the cards div */
-    divCheckbox.appendChild(firstDiv);
-    divValue.appendChild(secondDiv);
+  const checkboxDiv = document.createElement("div");
+  
+  const taskTextDiv = document.createElement("div");
+  taskTextDiv.className = "div_value";
 
-    /* Create and add the value inside the values div */
-    const pTask = document.createElement("p");
-    const pTaskContent = document.createTextNode(getTask);
+  const checkboxImage = document.createElement("img");
+  checkboxImage.className = "img_checkbox";
+  checkboxImage.src = urlCheckboxNotDone;
 
-    secondDiv.appendChild(pTask);
-    pTask.appendChild(pTaskContent);
-    count++;
-    /* Clean the input after the 'enter' */
-    inputText.value = "";
+  const taskParagraph = document.createElement("p");
+  taskParagraph.textContent = taskText;
+
+  checkboxDiv.appendChild(checkboxImage);
+  taskTextDiv.appendChild(taskParagraph);
+  taskDiv.appendChild(checkboxDiv);
+  taskDiv.appendChild(taskTextDiv);
+
+  divCheckbox.appendChild(taskDiv);
+}
+
+function handleEnterKey(event) {
+  if (event.key === "Enter") {
+    const taskText = inputText.value.trim();
+    if (taskText) {
+      createTask(taskText);
+      inputText.value = "";
+    }
   }
-});
+}
 
-/* Creating LocalStorage */
+function clearLocalStorage() {
+  localStorage.clear();
+}
 
-/* Creating the cards of the tasks */
+function taskDone() {
+  if (checkboxImage.src == urlCheckboxNotDone) {
+    checkboxImage.src = urlCheckboxDone;
+    taskParagraph.style.textDecoration = "line-through";
+  } else {
+    checkboxImage.src = urlCheckboxNotDone;
+  }
+}
+
+// Adiciona os event listeners
+divTaskList.addEventListener("click", toggleTaskDiv);
+inputText.addEventListener("keypress", handleEnterKey);
+clear.addEventListener("click", clearLocalStorage);
+taskDiv.addEventListener("click", taskDone);
